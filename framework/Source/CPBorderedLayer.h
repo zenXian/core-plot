43e23b330291690f426a577fd1cc011d20fb4171
Drew McCormack <drewmccormack@mac.com>
2009-10-15 15:31:20 +0200

    Changed the name of the tickLabelFormatter to axisLabelFormatter, to conform with other property names.
    Also added a rotation parameter for CPAxisLabel, and a similar
    parameter in CPAxis. This can be used to rotate axis labels by
    any angle (radians).

---
+++ framework/Source/CPBorderedLayer.h
@@ -0,0 +1,21 @@
+
+#import <Foundation/Foundation.h>
+#import "CPLayer.h"
+
+@class CPLineStyle;
+@class CPFill;
+
+@interface CPBorderedLayer : CPLayer {
+	@private
+	CPLineStyle *borderLineStyle;
+    CPFill *fill;
+	CGFloat cornerRadius;
+	CGPathRef outerBorderPath;
+	CGPathRef innerBorderPath;
+}
+
+@property (nonatomic, readwrite, copy) CPLineStyle *borderLineStyle;
+@property (nonatomic, readwrite, assign) CGFloat cornerRadius;
+@property (nonatomic, readwrite, copy) CPFill *fill;
+
+@end
