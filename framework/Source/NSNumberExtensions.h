274499269d675714b5f893b4f1906f5016313db9
Victor Martin <victor.martin.garcia@gmail.com>
2011-05-19 11:03:27 +0200

    Fixed issue #285

---
+++ framework/Source/NSNumberExtensions.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+/**	@category NSNumber(CPTExtensions)
+ *	@brief Core Plot extensions to NSNumber.
+ **/
+@interface NSNumber(CPTExtensions)
+
+-(NSDecimalNumber *)decimalNumber;
+
+@end
+
